c23985a08b4a6a44a17671e2ead0f54a249d7b97
ssranjani <ranjaniblooms@gmail.com>
2023-12-17 18:00:42 -0500

    First commit with springboot config

---
+++ src/test/java/com/demo/cloudapi/CloudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.cloudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
