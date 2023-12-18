1d893cc5d48c196ce7bcd0a4372342d352928604
ssranjani <ranjaniblooms@gmail.com>
2023-12-17 19:26:48 -0500

    Allow cors

--- src/main/java/com/demo/cloudapi/controller/DemoController.java
+++ src/main/java/com/demo/cloudapi/controller/DemoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/gifts")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class DemoController {
     @GetMapping("")
     public ResponseEntity<Integer> getGiftsCount(){
